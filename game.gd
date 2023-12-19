b353523859c1d58f8837f926271e3a93e53e5848
cope-de-grace <cruel_dilemme@mail.ru>
2023-12-19 14:11:45 +0300

    add level completion
    Добавлено меню после убийства всех врагов с возможностью перехода на следующий уровень. Отображение счета не работает

--- game.gd
+++ game.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
 const  GAME_OVER_SCENE = preload("res://elements/ui/game_over/game_over.tscn")
+const LEVEL_COMPLETE_SCENE = preload("res://elements/ui/level_complete/level_complete.tscn")
 
 func _ready():
 	Events.lives_changed.connect(func(lives): check_game_over())
-	Events.enemy_died.connect(check_game_over)
+	Events.enemy_died.connect(check_level_complete)
 
 func check_game_over():
 	if Globals.lives <= 0:
 		add_child(GAME_OVER_SCENE.instantiate())
 		Globals.lives = 3
+
+func check_level_complete():
+	var enemies = get_tree().get_nodes_in_group("enemy_green")
+	if enemies.size() <= 1:
+		add_child(LEVEL_COMPLETE_SCENE.instantiate())
