aaa6ecc595f380b5dbd7eb56b3c836a5fb1ae5d8
Mark K <154290771+cope-de-grace@users.noreply.github.com>
2024-01-10 18:59:43 +0300

    fix enemy_projectile
    вражеский продж не следует за врагом

--- elements/enemy_green/enemy_green.gd
+++ elements/enemy_green/enemy_green.gd
@@ -5,6 +5,7 @@ const PROJECTILE_SCENE = preload("res://elements/enemy_projectile/enemy_projecti
 @onready var raycast_left := $RayCastLeft
 @onready var raycast_right := $RayCastRight
 @onready var animation_enemy_green := $AnimatedSprite2D
+@onready var enemy := $"."
 
 func _physics_process(_delta):
 	if raycast_left.is_colliding() or raycast_right.is_colliding():
@@ -20,6 +21,7 @@ func destroy():
 func shot():
 	var projectile = PROJECTILE_SCENE.instantiate()
 	add_child(projectile)
+	projectile.global_transform = enemy.global_transform
 
 func _on_animated_sprite_2d_animation_finished():
 	queue_free()
