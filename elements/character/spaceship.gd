983b7636721f6414eb9227657dbcc6685e47391d
Mark K <154290771+cope-de-grace@users.noreply.github.com>
2024-01-12 18:36:51 +0300

    fix spaceship, enemies
    добавлен дилей выстрела корабля, анимации смерти врагов подогнаны под дилей выстрела корабля

--- elements/character/spaceship.gd
+++ elements/character/spaceship.gd
@@ -5,10 +5,13 @@ const SPEED = 150.0
 
 @onready var animation = $AnimatedSprite2D
 
+var can_shoot = true
+
 func _physics_process(delta):
 	
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") && can_shoot:
 		shot()
+		can_shoot = false
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * SPEED
@@ -19,6 +22,10 @@ func shot():
 	projectile.global_position = global_position + Vector2(0,-13) 
 	add_child(projectile)
 	$ShotSound.play()
+	projectile.tree_exited.connect(on_projectile_destroyed)
+
+func on_projectile_destroyed():
+	can_shoot = true
 
 func take_damage():
 	Globals.change_lives(-1)
