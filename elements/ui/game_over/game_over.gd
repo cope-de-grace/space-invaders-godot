05b4371075df5688484f5db7f12964b250175beb
Mark K <154290771+cope-de-grace@users.noreply.github.com>
2024-01-15 16:46:53 +0300

    fix buttons, score. mac export attempt

--- elements/ui/game_over/game_over.gd
+++ elements/ui/game_over/game_over.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 
+@onready var points_label = $MarginContainer2/VBoxContainer/HBoxContainer/Points
 
+func _ready():
+	Events.points_changed.connect(update_points)
+
+func update_points(points: int):
+	points_label.text = str(points)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
