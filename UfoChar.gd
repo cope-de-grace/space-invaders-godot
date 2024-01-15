8c66db9732cd2c90b66b7ba20ea2a9805493b686
Mark K <154290771+cope-de-grace@users.noreply.github.com>
2024-01-15 13:28:59 +0300

    update ufo
    добавлена анимация смерти уфо

--- UfoChar.gd
+++ UfoChar.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
+@onready var animation_ufo := $AnimatedSprite2D
+
 func destroy():
 	Globals.change_points(100)
 	Events.enemy_died.emit()
-	queue_free()
+	$"../Destroy".play()
+	animation_ufo.play("death")
+	animation_ufo.animation_finished
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+func _on_animated_sprite_2d_animation_finished():
 	queue_free()
